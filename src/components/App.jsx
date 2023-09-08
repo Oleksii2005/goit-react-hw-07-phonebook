b9d1c1ff6b9f0d4f33766f4f45a6e7cff2893e43
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-09-08 17:43:59 +0200

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { addContact, deleteContact, updateFilter } from '../redux/contactSlice';
+import { nanoid } from '@reduxjs/toolkit';
+import Notiflix from 'notiflix';
+
 export const App = () => {
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
+
+  const filteredContacts = contacts.filter(
+    contact =>
+      contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const addContactHandler = ({ name, number }) => {
+    const existingContact = contacts.find(
+      contact =>
+        contact.name && contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (existingContact) {
+      Notiflix.Notify.failure(`${name} is already in contacts`);
+      return;
+    }
+
+    dispatch(addContact({ id: nanoid(), name, number }));
+  };
+
+  const filterChangeHandler = e => {
+    dispatch(updateFilter(e.target.value));
+  };
+
+  const onDelete = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={addContactHandler} />
+      </Section>
+
+      <Section title="Contacts">
+        <Filter filter={filter} handleChange={filterChangeHandler} />
+        <ContactList contacts={filteredContacts} onDelete={onDelete} />
+      </Section>
+    </>
   );
 };
---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactsReducer from './contactSlice';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistedReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
